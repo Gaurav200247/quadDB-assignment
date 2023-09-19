75f04ce7e601b99dcb2856feab0afcd4cfc2a10b
Gaurav Gupta <gauravgupta742002@gmail.com>
2023-09-19 23:56:14 +0530

    bug fix 6

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require("path");
 const app = express();
 
 // static files
-app.use("/", express.static(path.join(__dirname, "/public")));
+// app.use("/", express.static(path.join(__dirname, "/public")));
 
 // api routes
 app.get("/", (req, res) => {
